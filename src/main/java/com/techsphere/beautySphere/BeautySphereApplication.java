44a94172e1300d50a4a5b4e8c04ab92a195a2c6a
leticia-farias <leticiafarias.engsoftware@gmail.com>
2025-05-26 17:08:38 -0300

    First commit

---
+++ src/main/java/com/techsphere/beautySphere/BeautySphereApplication.java
@@ -0,0 +1,13 @@
+package com.techsphere.beautySphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautySphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techsphere/beautySphere/BeautySphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsphere.beautySphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
