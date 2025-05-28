1731647f2e84c3f4f4746deb0c878f20431f1e60
Letícia Farias <95892733+leticia-farias@users.noreply.github.com>
2025-05-28 01:18:05 +0000

    addCliente

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,17 +1,23 @@
-function adicionarUsuario() {
+function addAgendamento() {
     const hora = document.getElementById("event-time").value;
     const nome = document.getElementById("event-input").value;
+    const telefone = document.getElementById("event-tel").value;
     const servico = document.getElementById("event-service").value;
 
     fetch('/api/agenda', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ hora, nome, servico })
-    })
-        .then(() => {
-            document.getElementById("hora").value = '';
-            document.getElementById("nome").value = '';
-            document.getElementById("servico").value = '';
-            carregarAgenda();
-        });
+    });
+}
+
+function addCliente() {
+    const nome = document.getElementById("event-input").value;
+    const telefone = document.getElementById("event-tel").value;
+    fetch('/api/clientes', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ nome, telefone })
+    });
+
 }
